73a03a4b1138362c9ad91772b8312f253ff30a92
chocochicka <120452647+chocochicka@users.noreply.github.com>
2023-08-25 12:50:32 +0700

    Update scheduling.py

--- scheduling.py
+++ scheduling.py
@@ -1,5 +1,8 @@
 import time
 
-while True:
+start_time = time.time()
+end_time = start_time + 60  # 60 seconds = 1 minute
+
+while time.time() < end_time:
     print("Hello World")
     time.sleep(15)
